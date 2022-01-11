3b96e0e984653f44775b5ce32788c35fadd82608
Casp3474 <79079388+Casp3474@users.noreply.github.com>
2022-01-11 19:58:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+  /*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+sr.reveal('#home h1, #home, .skills__subtitle, .skills__text',{}); 
+
+sr.reveal('#about, .work__img, .contact__input',{interval: 200}); 
+
+
+
+
